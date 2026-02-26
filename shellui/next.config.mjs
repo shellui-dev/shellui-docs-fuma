bdce459ed2b126cc1a377f83a1340a9717b7b5a1
Shewart <shewartshepherd23@gmail.com>
2026-02-26 22:47:00 +0200

    feat: add redirects for documentation versioning in next.config.mjs

--- shellui/next.config.mjs
+++ shellui/next.config.mjs
@@ -5,6 +5,25 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/docs",
+        destination: "/docs/v0.2.x",
+        permanent: false,
+      },
+      {
+        source: "/docs/v0.30-alpha1/:path*",
+        destination: "/docs/v0.3.0-alpha.2/:path*",
+        permanent: true,
+      },
+      {
+        source: "/docs/v0.3.0-alpha.1/:path*",
+        destination: "/docs/v0.3.0-alpha.2/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
